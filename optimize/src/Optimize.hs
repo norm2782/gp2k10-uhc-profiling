10439bac1d8dfbfd82d1c230237fac5f26467658
Henk Erik van der Hoek <mail@henkerikvanderhoek.nl>
2010-11-11 15:53:26 +0100

    Program up till definition 4.9, but untested

--- optimize/src/Optimize.hs
+++ optimize/src/Optimize.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE EmptyDataDecls #-}
-
 module Optimize where
 
 import Control.Monad.State 
@@ -15,24 +12,6 @@ data Expr = Var String
           | App String [Expr]
           | Case String [(Expr, Expr)]
           deriving Show        
-  
-{-
-Attempt to use a GADT
-
-data Var
-data Con
-data Fun 
-data App
-data Case
-          
-data Expr a where
-    Var  :: String                           -> Expr Var
-    Con  :: Name     -> [Expr a]             -> Expr Con
-    Fun  :: Name     -> [Expr a]             -> Expr Fun
-    App  :: Expr Var -> [Expr a]             -> Expr App
-    Case :: Expr Var -> [(Expr Con, Expr a)] -> Expr Case
-    deriving Show
--}
 
 -- Function definition
 data Def = Def Name [Expr] Expr deriving Show
@@ -45,7 +24,7 @@ raise :: Expr -> [Expr] -> State [Def] Expr
 raise (Var x) ee                  = return $ App x ee
 raise (Cons n dd) ee              = return $ Cons n (dd ++ ee)
 raise (Func n dd) ee | delta <= 0 = return $ Func n (dd ++ ee)
-                     | otherwise  = error "Not yet implemented" -- State $ \state -> (Func (increase n delta) (dd ++ ee), (increase n delta, raise ))
+                     | otherwise  = State $ \state -> (Func (increase n delta) (dd ++ ee), (Def (name "raise" 0) [] (Var "r")):state)
                                   where
                                       delta = length dd + length ee - arity n                                  
 raise (App x dd) ee               = return $ App x (dd ++ ee)
@@ -59,35 +38,48 @@ raise (Case x tuples) ee          = mmap update tuples >>= wrap
         update (pattern, expr) = raise expr ee >>= (\updated -> return $ (pattern, updated))
 
 
-data Subst = Subst String Expr
+-- Substitute      new  old
+data Subst = Subst Expr String
 
-        
+
+
 apply :: Subst -> String -> Expr 
-apply = undefined    
+apply (Subst new old) for | old == for = new
+                          | otherwise  = Var for
 
                                     
 -- Definition 4.7
 substitute :: Subst -> Expr -> State [Def] Expr 
 substitute s (Var x)      = return $ apply s x
+substitute s (App x ee)   = mmap (substitute s) ee >>= raise (apply s x)
 substitute s (Func st ee) = mmap (substitute s) ee >>= wrap
     where
         wrap ff = return $ Func st ff
 substitute s (Cons st ee) = mmap (substitute s) ee >>= wrap
     where
         wrap ff = return $ Cons st ff
-substitute s (App x ee)   = mmap (substitute s) ee >>= raise (apply s x)
+
 
 
 
 -- Definition 4.8
 match :: Int -> Expr -> Def -> State [Def] Expr
-match i (Var y)      (Def name arguments (Case x tuples)) = return $ Case y tuples
-match i f@(Func _ _) (Def name arguments (Case x tuples)) = return $ Func name ((take i arguments) ++ [f] ++ (drop (i + 1) arguments))
-match i a@(App _ _)  (Def name arguments (Case x tuples)) = return $ Func name ((take i arguments) ++ [a] ++ (drop (i + 1) arguments))
-match i (Cons n ee)  (Def name arguments (Case x tuples)) = undefined -- substitute subst expr
+match i (Var y)         (Def name arguments (Case x tuples)) = return $ Case y tuples
+match i f@(Func _ _)    (Def name arguments (Case x tuples)) = return $ Func name ((take i arguments) ++ [f] ++ (drop (i + 1) arguments))
+match i a@(App _ _)     (Def name arguments (Case x tuples)) = return $ Func name ((take i arguments) ++ [a] ++ (drop (i + 1) arguments))
+match i (Case y tuples) definition = mmap update tuples >>= wrap
     where
-        (Cons _ yy, expr) = head $ filter (\((Cons m _), _) -> m == n) tuples
-        subst = undefined 
+        wrap tuples = return $ Case y tuples
+        
+        update :: (Expr, Expr) -> State [Def] (Expr, Expr)
+        update (pattern, expr) = (match i expr definition) >>= (\updated -> return $ (pattern, updated))
+
+match i (Cons n ee)  (Def name arguments (Case x tuples)) = foldl (>>=) (return a) substitutions
+    where
+        (Cons _ yy, a) = head $ filter (\((Cons m _), _) -> m == n) tuples
+        
+        substitutions :: [Expr -> State [Def] Expr]
+        substitutions = map substitute $ map (\(e, (Var y)) -> Subst e y) $ zip ee yy
             
 
 
@@ -105,10 +97,21 @@ fuse f@(Def fn xx (Case x tuples)) s@(Def sn zz b) i = State $ \state -> ((), [D
 
 
 -- 1b & 2: TODO
-fuse _ _ _ = undefined
+fuse _ _ _ = error "fuse: not yet implemented"
 
 
-main = (putStrLn . show) $ runState (fuse (Def (name "F" 2) [Var "x", Var "y"] (Case "x" [])) (Def (name "S" 1) [Var "z"] (Var "z")) 2) []
+app :: Def
+app = Def (name "app" 2) [Var "l", Var "t"] 
+    (Case "l" [
+        (Cons (name "Nil" 0) [], Var "t"),
+        (Cons (name "Cons" 2) [Var "x", Var "xs"], Cons (name "Cons" 2) [Var "x", Func (name "app" 2) [Var "xs", Var "t"]])
+    ])
+    
+foo :: Def
+foo = Def (name "foo" 3) [Var "x", Var "y", Var "z"] 
+    (Func (name "app" 2) [Func (name "app" 2) [Var "x", Var "y"], Var "z"]) 
+
+main = (putStrLn . show) $ runState (fuse app app 1) []
 
 
 
